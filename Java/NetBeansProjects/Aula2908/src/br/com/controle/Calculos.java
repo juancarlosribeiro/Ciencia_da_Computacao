6e8ba9bc9c53b491483f4081de52a70c94b87c54
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2024-08-29 11:13:54 -0300

    upload

---
+++ Java/NetBeansProjects/Aula2908/src/br/com/controle/Calculos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author juan.fiuza
+ */
+public class Calculos {
+     
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    public double calcularDobro (){
+        return this.valor*2;
+    }
+}
