d60b700eddcf022cd4251f6aacb78f6cbd38cdb4
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2024-09-19 10:36:07 -0300

    Aula de Polimorfismo e Sobrecarga

---
+++ Java/NetBeansProjects/Polimorfismo/src/br/com/controle/OperacaoMatematics.java
@@ -0,0 +1,21 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author juan.fiuza
+ */
+public class OperacaoMatematics {
+    public Double calcular(Double valor1, Double valor2){
+        return 0.0;
+    
+    }
+    
+    
+    
+    
+    
+}
