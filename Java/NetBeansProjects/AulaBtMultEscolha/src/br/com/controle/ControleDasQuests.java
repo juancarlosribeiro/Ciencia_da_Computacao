9a38b579939265b6eb1f0dd7a760d006c2a1a007
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2024-10-03 11:15:51 -0300

    Aula Terminar

---
+++ Java/NetBeansProjects/AulaBtMultEscolha/src/br/com/controle/ControleDasQuests.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+/**
+ *
+ * @author juan.fiuza
+ */
+public class ControleDasQuests {
+    private int opcao;
+    int soma;
+       public void setOpcao(int opcao) {
+        this.opcao =opcao;
+         soma++;  
+    }
+       public int somar(){
+         return soma;
+      }
+    
+}
+
+
+
