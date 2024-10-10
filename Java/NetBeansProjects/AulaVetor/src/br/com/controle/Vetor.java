ef881a9e22ed466cf1df40ff4fba56a6777f3035
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2024-10-10 11:26:41 -0300

    Aula vetores

---
+++ Java/NetBeansProjects/AulaVetor/src/br/com/controle/Vetor.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.controle;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author juan.fiuza
+ */
+/*public class Vetor {
+    public static void main(String[] args){
+    
+    int codigo;
+    String nome = null;
+    int idade = 0;
+    int cont;
+    
+    ArrayList<String> cadastro = new ArrayList<>();
+    for(int c = 0; c < 2; c++){
+    codigo=Integer.valueOf(JOptionPane.showInputDialog("Digite o codigo: ")) ;
+    nome=JOptionPane.showInputDialog("Digite o nome: ");
+    idade=Integer.valueOf(JOptionPane.showInputDialog("Digite o idade: ")) ;
+    
+    cadastro.add(String.valueOf(codigo));
+    cadastro.add(nome);
+    cadastro.add(String.valueOf(idade));
+    
+    
+    
+    }
+    for(int c = 0; c < 2; c++){
+    
+        JOptionPane.showMessageDialog(null, cadastro.toString());
+    
+    
+    }
+
+    
+    
+    }
+}
+*/
