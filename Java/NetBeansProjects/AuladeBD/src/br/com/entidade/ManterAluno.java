9df0042641b24b99b8855262efbfe79586d51f4c
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2024-10-31 11:02:30 -0300

    Sobe

--- Java/NetBeansProjects/AuladeBD/src/br/com/entidade/ManterAluno.java
+++ Java/NetBeansProjects/AuladeBD/src/br/com/entidade/ManterAluno.java
@@ -5,8 +5,10 @@
 package br.com.entidade;
 
 import br.com.controle.Alunos;
+import javax.swing.JOptionPane;
 import java.sql.PreparedStatement;
-
+import java.sql.ResultSet;
+import java.util.ArrayList;
 /**
  *
  * @author juan.fiuza
@@ -15,7 +17,7 @@ public class ManterAluno extends DAO {
     public void inserir(Alunos a) throws Exception {
     try {
     abrirBanco();
-    String query = "INSERT INTO alunos(codigo,nome,email)" + "values(null,?,?)";
+    String query = "INSERT INTO aluno(codigo,nome,email)" + "values(null,?,?)";
     pst=(PreparedStatement) con.prepareStatement(query);
     pst.setString(1, a.getNome());
     pst.setString(2, a.getEmail());
@@ -25,8 +27,32 @@ public class ManterAluno extends DAO {
     catch (Exception e){
         System.out.println("Erro" + e.getMessage());
     }
-    
-    
+    }
+    /**
+     *
+     * @return
+     * @throws Exception
+     */
+    public ArrayList<Alunos> PesquisarTudo() throws Exception{
+       ArrayList<Alunos> alunos = new ArrayList<Alunos>();
+         try{
+         abrirBanco();  
+         String query = "select codigo, nome FROM alunos";
+         pst = (PreparedStatement) con.prepareStatement(query);
+         ResultSet tr = pst.executeQuery();
+         Alunos a ;
+         while (tr.next()){               
+           a = new Alunos();
+           a.setCodigo(tr.getInt("codigo"));
+           a.setNome(tr.getString("nome"));
+           alunos.add(a);
+         } 
+         fecharBanco();
+       }catch (Exception e){
+           System.out.println("Erro " + e.getMessage());
+     } 
+       return alunos;
+     }
     
     
     
@@ -43,4 +69,11 @@ public class ManterAluno extends DAO {
     
     }
     
-}
+
+
+
+
+
+
+
+    
