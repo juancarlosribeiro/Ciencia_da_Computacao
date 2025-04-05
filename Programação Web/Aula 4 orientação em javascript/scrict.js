811383371467cd8548c8cf82ee88e27c9fbff025
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2025-04-05 11:10:45 -0300

    Aula orientação a objetos

---
+++ Programação Web/Aula 4 orientação em javascript/scrict.js
@@ -0,0 +1,16 @@
+const pessoa = {
+
+    nome: "Pedro",
+    idade: 20
+
+
+};
+
+console.log(pessoa);
+
+let pessoa2 = {};
+pessoa2.nome= "Mario";
+pessoa2.idade = 70;
+pessoa2.altura = 1.75;
+
+console.log(pessoa2);
