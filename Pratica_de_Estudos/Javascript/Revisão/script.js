7d4cd242275c8208f6e04171f4a57def1e9f0cf8
Juan Carlos Ribeiro Fiuza <juancarlosribeiro167@gmail.com>
2025-04-11 21:47:27 -0300

    Revisão

---
+++ Pratica_de_Estudos/Javascript/Revisão/script.js
@@ -0,0 +1,34 @@
+var animal = "Gato";
+console.log(animal);
+
+let bicho = "Onça";
+console.log(bicho);
+
+const carroBom = "Supra";
+console.log(carroBom);
+
+
+animal = "Dog";
+bicho = "Baleia";
+
+console.log(animal);
+console.log(bicho);
+
+/*const = "Tesla";
+console.log(carroBom);*/
+
+//aqui é pra dar erro pois constantes não mudam  por isso tá comentado
+
+var idade = "20";
+var nome = "Ana Luísa"
+
+console.log(`O nome dela é ${nome} e ela tem ${idade} anos de idade`);
+//aqui pra funcionar tem que usar crase simples sem ser dupla. Assim se declara em Javascript
+
+console.log(5 == "5"); // aqui significa igual com dois ==
+console.log(5 === "5"); // aqui significa estritamente igual e compara tudo inclusive o tipo de varialvel portanto vai dar false
+
+console.log(5 != "5"); // aqui significa DESigual ou seja se são diferentes, aqui dá false pois são "Iguais"
+console.log(5 !== "5"); // aqui significa estritamente DESigual ou seja se são diferentes, ou se são extremamente iguais ou se der alguma diferença minima o programa da true
+
+
