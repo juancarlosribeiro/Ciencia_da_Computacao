9995a3b72a30d8cce9a69963dbcc54ee48328b7f
JUAN CARLOS RIBEIRO FIUZA <juan.fiuza@a.ucb.br>
2025-04-25 10:47:59 -0300

    codigo faltante de orientação em python

---
+++ Python/Novas tecnologias/Orientação a objetos/contato.py
@@ -0,0 +1,10 @@
+class contato():
+    
+    
+    __slots__ =['nome', 'telefone','datanasc','email']
+ 
+    def __init__(self,nome,telefone,datanasc,email):
+        self .nome=nome
+        self .telefone=telefone
+        self .datanasc=datanasc
+        self .email=email
---
+++ Python/Novas tecnologias/Orientação a objetos/main.py
@@ -0,0 +1,16 @@
+import contato
+
+import datetime
+
+print("""
+      ***************************************
+      *      Bem vindo a sua agenda         *
+      ***************************************
+      
+      """)
+
+nome = "Tung Sahur"
+telefone = "(61)99335-3201"
+datanasc  = datetime.date(2000,9,15)
+email = "tungtung@gmail.com"
+
